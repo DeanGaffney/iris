1ac8e5ac228b5ecad7ad8839498da7e71ea58326
Dean <gaffmasterflex123@gmail.com>
2017-10-25 01:39:03 +0100

    added Elasticsearch service, and Crud functionality for elasticsearch indices when a Schema is being saved,updated or deleted

---
+++ grails-app/services/com/wit/iris/elastic/ElasticService.groovy
@@ -0,0 +1,81 @@
+package com.wit.iris.elastic
+
+import com.wit.iris.schemas.Schema
+import grails.gorm.transactions.Transactional
+import grails.plugins.rest.client.RestBuilder
+import grails.plugins.rest.client.RestResponse
+
+@Transactional
+class ElasticService {
+
+    final String endpoint = "https://search-iris-ibwkuxcv4b2unly3c7d2d77v2a.eu-west-1.es.amazonaws.com"
+
+    RestBuilder rest = new RestBuilder()
+    RestResponse resp
+
+    /**
+     * Creates an Elasticsearch inde
+     * @param schema
+     */
+    void createIndex(Schema schema){
+        //create index and mapping at the same time in one call
+        resp = rest.put("$endpoint/$schema.esIndex"){
+            contentType "application/json"
+            json createMapping()
+        }
+
+        //TODO see if the status is ok from elasticsearch, or else throw an exception
+    }
+
+    /**
+     * Creates a mapping for schema, json format is specified below
+     * PUT indexName
+     {
+        "mappings": {
+            "schema": {
+                "properties": {
+                    "name": {
+                        "type": "text"
+                    },
+                    "fieldType":{
+                        "type" : "text"
+                    }
+                }
+            }
+        }
+     }
+     */
+    String createMapping(){
+        return "{\n" +
+                "        \"mappings\": {\n" +
+                "            \"schema\": {\n" +
+                "                \"properties\": {\n" +
+                "                    \"name\": {\n" +
+                "                        \"type\": \"text\"\n" +
+                "                    },\n" +
+                "                    \"fieldType\":{\n" +
+                "                        \"type\" : \"text\"\n" +
+                "                    }\n" +
+                "                }\n" +
+                "            }\n" +
+                "        }\n" +
+                "     }"
+    }
+
+    /**
+     * Deletes a schemas elasticsearch index
+     * @param schema, the schemas index to delete
+     */
+    void deleteIndex(Schema schema){
+        resp = rest.delete("$endpoint/$schema.esIndex")
+    }
+
+    /**
+     * Creates a suitable index name from a Schema name
+     * @param schemaName
+     * @return The elasticsearch index string
+     */
+    String getIndexFromName(String schemaName){
+        return schemaName.toLowerCase().replaceAll(" ", "_")
+    }
+}
--- grails-app/services/com/wit/iris/schemas/SchemaService.groovy
+++ grails-app/services/com/wit/iris/schemas/SchemaService.groovy
@@ -6,20 +6,28 @@ import grails.gorm.transactions.Transactional
 class SchemaService {
 
     def springSecurityService
+    def elasticService
 
+    /**
+     * Creates a schema domain and its respective elasticsearch index
+     * @param schema, the schema to save
+     * @return the saved schema instance
+     */
     Schema createSchema(Schema schema){
-        schema.esIndex = getEsIndexFromName(schema.name)
+        schema.esIndex = elasticService.getIndexFromName(schema.name)
         schema.user = springSecurityService.getCurrentUser()
         if(!(schema.validate() && schema.save(flush: true))){
             println(schema.errors)
         }
+        elasticService.createIndex(schema)     //create elastic search index and mapping
         return schema
     }
 
-    String getEsIndexFromName(String name){
-        return name.toLowerCase().replaceAll(" ", "_")
-    }
-
+    /**
+     * Updates a schema domain and its elasticsearch mapping
+     * @param schema, the schema to update
+     * @return The updated version of the schema
+     */
     Schema updateSchema(Schema schema){
         Schema updatedSchema = Schema.findByName(schema.name)
         updatedSchema.properties = schema.properties
@@ -29,8 +37,15 @@ class SchemaService {
         return updatedSchema
     }
 
+    /**
+     * Deletes a schema and its elasticsearch index
+     * @param schema, the schema to delete
+     */
     void deleteSchema(Schema schema){
-        schema.delete(flush: true)
+        elasticService.deleteIndex(schema)      //delete the elasticsearch index
+        if(!schema.delete(flush: true)){
+            println(schema.errors)
+        }
     }
 
 }
