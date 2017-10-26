d5d0ca7c832271c4e374c1e100d82b563bdcf1b4
Dean _Gaffney <gaffmasterflex123@gmail.com>
2017-10-26 09:41:10 +0100

    added rest service for sending data to Elasticsearch

--- grails-app/services/com/wit/iris/rest/RestService.groovy
+++ grails-app/services/com/wit/iris/rest/RestService.groovy
@@ -10,6 +10,12 @@ class RestService {
     RestBuilder rest = new RestBuilder()
     RestResponse resp
 
+    /**
+     * REST PUT method
+     * @param url - the url to act on
+     * @param jsonData - the json data in string format to send to the url
+     * @return status code of the response
+     */
     int put(String url, String jsonData){
         resp = rest.put(url){
             contentType "application/json"
@@ -19,12 +25,20 @@ class RestService {
         return resp.statusCodeValue
     }
 
+    /**
+     * REST DELETE method
+     * @param url - the url to act on
+     * @return status code of the response
+     */
     int delete(String url){
         resp = rest.delete(url)
         logResponse()
         return resp.statusCodeValue
     }
 
+    /**
+     * Logs out the status code and json from the current response object
+     */
     void logResponse(){
         println("Status code : $resp.statusCodeValue")
         println("Json response: ${resp.json.toString()}")
