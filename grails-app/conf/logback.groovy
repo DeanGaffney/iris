249f4df920ea01f5a370fda76f875cf7647f0ccc
Dean <gaffmasterflex123@gmail.com>
2018-02-04 22:06:43 +0000

    reverted some app config to run app in prod

--- grails-app/conf/logback.groovy
+++ grails-app/conf/logback.groovy
@@ -32,15 +32,16 @@ if (Environment.isDevelopmentMode() && targetDir != null) {
         }
     }
     logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false)
-}
-
-appender("IRIS_DEBUG", FileAppender){
-    file = "${targetDir}/debug.log"
-    append = true
-    encoder(PatternLayoutEncoder) {
-        pattern = "%level %logger - %msg%n"
+    appender("IRIS_DEBUG", FileAppender){
+        file = "${targetDir}/debug.log"
+        append = true
+        encoder(PatternLayoutEncoder) {
+            pattern = "%level %logger - %msg%n"
+        }
     }
+    logger("com.wit.iris", DEBUG, ['IRIS_DEBUG'], false)
 }
-logger("com.wit.iris", DEBUG, ['IRIS_DEBUG'], false)
 
-root(ERROR, ['STDOUT', 'IRIS_DEBUG'])
+
+
+root(ERROR, ['STDOUT'])
