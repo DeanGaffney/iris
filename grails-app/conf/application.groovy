2ee0255c39f22623935123dc5852db81dd4473dc
Dean <deanpatrickgaffney@gmail.com>
2018-03-07 11:15:13 +0000

    updated the application.groovy file for rest settings

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -4,32 +4,45 @@
 grails.plugin.springsecurity.userLookup.userDomainClassName = 'com.wit.iris.users.User'
 grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'com.wit.iris.users.UserRole'
 grails.plugin.springsecurity.authority.className = 'com.wit.iris.users.Role'
-grails.plugin.springsecurity.controllerAnnotations.staticRules = [
-	[pattern: '/',               access: ['permitAll']],
-	[pattern: '/error',          access: ['permitAll']],
-	[pattern: '/index',          access: ['permitAll']],
-	[pattern: '/index.gsp',      access: ['permitAll']],
-	[pattern: '/shutdown',       access: ['permitAll']],
-	[pattern: '/assets/**',      access: ['permitAll']],
-	[pattern: '/**/js/**',       access: ['permitAll']],
-	[pattern: '/**/css/**',      access: ['permitAll']],
-	[pattern: '/**/images/**',   access: ['permitAll']],
-	[pattern: '/**/favicon.ico', access: ['permitAll']],
-	[pattern: '/schema/**', 	 access: ['permitAll']],
-	[pattern: '/schemaField/**', access: ['permitAll']],
-	[pattern: '/dashboard/**', 	 access: ['permitAll']],
-	[pattern: '/aggregation/**', access: ['permitAll']],
-	[pattern: '/elasticEndpoint/**', access: ['permitAll']],
-	[pattern: '/stomp/**', 	 	 access: ['permitAll']]
+grails.plugin.springsecurity.securityConfigType = 'InterceptUrlMap'
+grails.plugin.springsecurity.interceptUrlMap = [
+		[pattern: '/',               access: ['permitAll']],
+		[pattern: '/error',          access: ['permitAll']],
+		[pattern: '/index',          access: ['permitAll']],
+		[pattern: '/index.gsp',      access: ['permitAll']],
+		[pattern: '/shutdown',       access: ['permitAll']],
+		[pattern: '/assets/**',      access: ['permitAll']],
+		[pattern: '/**/js/**',       access: ['permitAll']],
+		[pattern: '/**/css/**',      access: ['permitAll']],
+		[pattern: '/**/images/**',   access: ['permitAll']],
+		[pattern: '/**/favicon.ico', access: ['permitAll']],
+		[pattern: '/login/**',       access: ['permitAll']],
+		[pattern: '/api/login',      access: ['permitAll']],
+		[pattern: '/api/logout',     access: ['isFullyAuthenticated()']],
+		[pattern: '/schema/getAgentUrl',     access: ['isFullyAuthenticated()']],
+		[pattern: '/schema/route/**',      access: ['isFullyAuthenticated()']],
+		[pattern: '/**',             access: ['isFullyAuthenticated()']]
 
 ]
 
 grails.plugin.springsecurity.filterChain.chainMap = [
-	[pattern: '/assets/**',      filters: 'none'],
-	[pattern: '/**/js/**',       filters: 'none'],
-	[pattern: '/**/css/**',      filters: 'none'],
-	[pattern: '/**/images/**',   filters: 'none'],
-	[pattern: '/**/favicon.ico', filters: 'none'],
-	[pattern: '/**',             filters: 'JOINED_FILTERS']
+
+		[pattern: '/api/**', filters:'JOINED_FILTERS,-anonymousAuthenticationFilter,-exceptionTranslationFilter,-authenticationProcessingFilter,-securityContextPersistenceFilter'],
+
+		[pattern: '/schema/route/**', filters:'JOINED_FILTERS,-anonymousAuthenticationFilter,-exceptionTranslationFilter,-authenticationProcessingFilter,-securityContextPersistenceFilter'],
+
+		[pattern: '/**', filters:'JOINED_FILTERS,-restTokenValidationFilter,-restExceptionTranslationFilter']
+
 ]
 
+grails.plugin.springsecurity.rest.logout.endpointUrl = '/api/logout'
+
+grails.plugin.springsecurity.rest.token.validation.useBearerToken = false
+
+grails.plugin.springsecurity.rest.token.validation.headerName = 'X-Auth-Token'
+
+grails.plugin.springsecurity.rest.token.storage.gorm.tokenDomainClassName = 'com.wit.iris.auth.AuthenticationToken'
+
+grails.plugin.springsecurity.rest.token.storage.gorm.tokenValuePropertyName = 'tokenValue'
+
+grails.plugin.springsecurity.rest.token.storage.gorm.usernamePropertyName = 'username'
